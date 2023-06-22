eaab9d78df0b20da23f82a2dc719e1238341e225
Povilas <povilaskazlauskas2003@gmail.com>
2023-06-22 23:48:03 +0200

    stuff

--- src/databaseFunctions.js
+++ src/databaseFunctions.js
@@ -2,20 +2,11 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
 import {getDatabase, ref, set} from "firebase/database";
-
+import { firebaseConfig } from "./firebaseConfig";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCB3J0LwARy63k-xBe4sSYfEvSwGwFnYrg",
-  authDomain: "component-list-2f893.firebaseapp.com",
-  databaseURL: "https://component-list-2f893-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "component-list-2f893",
-  storageBucket: "component-list-2f893.appspot.com",
-  messagingSenderId: "508101663973",
-  appId: "1:508101663973:web:dac7ee88984a800bb3c80c"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
